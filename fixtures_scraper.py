bc70e43fc608c8eed2cc74ae5714ea1bf3b4479a
gulfari1 <gulfaridaraz@gmail.com>
2025-03-05 05:48:45 +0500

    Update fixtures_scraper.py

--- fixtures_scraper.py
+++ fixtures_scraper.py
@@ -87,7 +87,7 @@ def fetch_fixtures():
     return data
 
 def save_fixtures(data):
-    with open('scores_fixtures.json', 'w', encoding='utf-8') as f:
+    with open('../data/scores_fixtures.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
 if __name__ == "__main__":
